1d3d2776fd32da09e18bb51cfecb8c518c565940
Jeanette Mikkelsen <jeanette.mikkelsen@utb.ecutbildning.se>
2025-05-30 20:39:12 +0200

    Implemented functionality for JWT tokens with full JWT-based authentication and authorization.

--- Business/Services/AccountHttpClient.cs
+++ Business/Services/AccountHttpClient.cs
@@ -9,7 +9,11 @@ public class AccountHttpClient(HttpClient http) : IAccountHttpClient
 {
     public async Task<ValidatedUserDto?> ValidateLoginAsync(LoginRequest request)
     {
-        var response = await http.PostAsJsonAsync("https://ventixe-accountserviceprovider-cdhuf2azb3end8eb.swedencentral-01.azurewebsites.net", request);
+        var response = await http.PostAsJsonAsync(
+            "https://ventixe-accountserviceprovider-cdhuf2azb3end8eb.swedencentral-01.azurewebsites.net/api/accounts/validate-login",
+            request
+        );
+
         if (!response.IsSuccessStatusCode)
             return null;
 
